0c8bf1d6ff8b209d234c2e630bfd389ae4f2f3f4
Leonid Zychkov <buldog52@gmail.com>
2019-01-21 23:04:42 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+window.$ = window.jQuery = require('jquery');
+
+// import './components/main-header/main-header';
+// import './components/modal/modal';
+// import './components/callback/callback';
+// import './components/sticky-menu/sticky-menu';
+// import './components/scroll-up/scroll-up';
